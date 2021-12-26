8ea1b0c050cdf211add0184676a8f70319970c0b
Manish Ghildiyal <manish.ghildiyal@lunatech.be>
2021-12-26 18:48:29 +0100

    First commit

---
+++ src/main/java/com/manish/learning/devopsforjavadeveloper/DevopsForJavaDeveloperApplication.java
@@ -0,0 +1,13 @@
+package com.manish.learning.devopsforjavadeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsForJavaDeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsForJavaDeveloperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manish/learning/devopsforjavadeveloper/DevopsForJavaDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manish.learning.devopsforjavadeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsForJavaDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
